2bd7d1266ad64741e5ca9c574c7889677da5cfc0
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2023-09-25 12:06:57 -0300

    resolving_fusion_branchs_problems

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,19 +1,29 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Hero from './Hero';
 
 const Fetch = () => {
   const APIkey = '3d9cbbaa2c744ad8b91912d8c0979261';
   const fetchData = async () => {
     // prettier-ignore
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${APIkey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=berazategui&units=metric&appid=${APIkey}`); //chequear city query param para poder buscar dinamicamente una locacion
     const data = await response.json();
     console.log(data);
 
     //forecast data
     const city = data.city.name;
+    const forecast = data.list[0].main;
+    const probabilityOfPrecipitation = data.list[1].pop * 100;
+    console.log(forecast);
 
     const weatherInfo = document.querySelector('.forecastData');
-    weatherInfo.innerHTML = `<h3>${city}</h3>`;
+    // prettier-ignore
+    weatherInfo.innerHTML = 
+    `<div>
+    <h3>${city}</h3>
+    <p>Current Temperature ${forecast.temp.toFixed(1)}°C</p>
+    <p>Humidity ${forecast.humidity}%</p>
+    <p>Probability of Precipitation ${probabilityOfPrecipitation}%</p>
+    </div>`;
   };
 
   return (
