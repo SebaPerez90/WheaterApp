f80b88f63a13db58b92b04bfd5343616b1b95436
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2023-10-05 11:11:08 -0300

    add-new-feature

--- src/components/BackgroundImg.jsx
+++ src/components/BackgroundImg.jsx
@@ -27,65 +27,63 @@ const BackgroundImg = ({ weatherData }) => {
   const renderingImg = (iconID) => {
     switch (iconID) {
       case '01d':
-        return <img src={imagesObj.d01} alt={weatherData.main} />;
+        return <img src={imagesObj.d01} alt={weatherData.main} className='backgroundImg' />;
 
       case '02d':
-        return <img src={imagesObj.d02} alt={weatherData.main} />;
+        return <img src={imagesObj.d02} alt={weatherData.main} className='backgroundImg' />;
 
       case '03d':
-        return <img src={imagesObj.d03} alt={weatherData.main} />;
+        return <img src={imagesObj.d03} alt={weatherData.main} className='backgroundImg' />;
 
       case '04d':
-        return <img src={imagesObj.d04} alt={weatherData.main} />;
+        return <img src={imagesObj.d04} alt={weatherData.main} className='backgroundImg' />;
 
       case '09d':
-        return <img src={imagesObj.d09} alt={weatherData.main} />;
+        return <img src={imagesObj.d09} alt={weatherData.main} className='backgroundImg' />;
 
       case '10d':
-        return <img src={imagesObj.d10} alt={weatherData.main} />;
+        return <img src={imagesObj.d10} alt={weatherData.main} className='backgroundImg' />;
 
       case '11d':
-        return <img src={imagesObj.d11} alt={weatherData.main} />;
+        return <img src={imagesObj.d11} alt={weatherData.main} className='backgroundImg' />;
 
       case '13d':
-        return <img src={imagesObj.d13} alt={weatherData.main} />;
+        return <img src={imagesObj.d13} alt={weatherData.main} className='backgroundImg' />;
 
       case '50d':
-        return <img src={imagesObj.d50} alt={weatherData.main} />;
+        return <img src={imagesObj.d50} alt={weatherData.main} className='backgroundImg' />;
 
       case '01n':
-        return <img src={imagesObj.n01} alt={weatherData.main} />;
+        return <img src={imagesObj.n01} alt={weatherData.main} className='backgroundImg' />;
 
       case '02n':
-        return <img src={imagesObj.n02} alt={weatherData.main} />;
+        return <img src={imagesObj.n02} alt={weatherData.main} className='backgroundImg' />;
 
       case '03n':
-        return <img src={imagesObj.n03} alt={weatherData.main} />;
+        return <img src={imagesObj.n03} alt={weatherData.main} className='backgroundImg' />;
 
       case '04n':
-        return <img src={imagesObj.n04} alt={weatherData.main} />;
+        return <img src={imagesObj.n04} alt={weatherData.main} className='backgroundImg' />;
 
       case '09n':
-        return <img src={imagesObj.n09} alt={weatherData.main} />;
+        return <img src={imagesObj.n09} alt={weatherData.main} className='backgroundImg' />;
 
       case '10n':
-        return <img src={imagesObj.n10} alt={weatherData.main} />;
+        return <img src={imagesObj.n10} alt={weatherData.main} className='backgroundImg' />;
 
       case '11n':
-        return <img src={imagesObj.n11} alt={weatherData.main} />;
+        return <img src={imagesObj.n11} alt={weatherData.main} className='backgroundImg' />;
 
       case '13n':
-        return <img src={imagesObj.n13} alt={weatherData.main} />;
+        return <img src={imagesObj.n13} alt={weatherData.main} className='backgroundImg' />;
 
       case '50n':
-        return <img src={imagesObj.n50} alt={weatherData.main} />;
+        return <img src={imagesObj.n50} alt={weatherData.main} className='backgroundImg' />;
     }
   };
 
   return (
-    <div className='contenedor-pepe'>
-      {renderingImg(weatherData.iconID)}
-    </div>
+    <div className='backgroundImg-container'>{renderingImg(weatherData.iconID)}</div>
   );
 };
 BackgroundImg.propTypes = {
