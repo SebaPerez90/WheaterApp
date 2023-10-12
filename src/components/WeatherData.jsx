20dc968d7417077ec188ea43db1bbe1cb32aaedf
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2023-10-11 21:36:01 -0300

    adding-search-history

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,11 +1,12 @@
 import SpanishCardInfo from './SpanishCardInfo.jsx';
 import EnglishCardInfo from './EnglishCardInfo.jsx';
 import ForecastNextDays from './ForecastNextDays.jsx';
+import History from './History.jsx';
 import { FcSearch } from 'react-icons/fc';
 import { Redirect } from 'wouter';
 import { useStore } from '../../store';
 import { toast } from 'react-hot-toast';
-import { useState, useEffect } from 'react';
+import { useRef } from 'react';
 
 export default function WeatherData() {
   const { shouldRedirect, languageEng, valueCapture, setValueCapture, setWeatherData, themeDark } = useStore();
@@ -13,56 +14,34 @@ export default function WeatherData() {
   const APIkey = '3d9cbbaa2c744ad8b91912d8c0979261';
   const URLDinamicRequest = `http://api.openweathermap.org/data/2.5/forecast?q=${valueCapture}&units=metric&appid=${APIkey}`;
 
+  //this is the ref child component which contains the method 'handleSetStorage' previously config
+  const historyRef = useRef();
+
+  // alert message in the case the input it is empty
   const errorMessage = () =>
     toast.error('text input can not be empty', {
       position: 'top-center',
     });
 
-  //test
-  const input = document.querySelector('.weather-input-dt');
-
+  // this function is trigger to the button 'search' to control the content in input type text
+  // and if it is all ok , trigger the fetch fn() and history search fn()
   const checkingInput = () => {
     if (!valueCapture) {
       errorMessage();
       return;
     } else {
       setWeatherData(URLDinamicRequest);
-      setStorage();
+      historyRef.current.handleSetStorage();
     }
-  };
-
-  //this effect run if the languageEng state change.
-  // useEffect(() => {
-  //   // if (valueCapture) {
-  //   // setweatherData();
-  //   // }
-  // }, [languageEng]);
-
-  //test
-  // useEffect(() => {
-  //   const storedHistory = JSON.parse(localStorage.getItem('history')) || [];
-  //   setHistory(storedHistory);
-  // }, []);
-
-  //test
-  const [history, setHistory] = useState([]);
-
-  const setStorage = () => {
-    localStorage.setItem('history', JSON.stringify(valueCapture));
-
-    setHistory([...history, valueCapture]);
-
-    input.value = '';
   };
 
   return (
     <main className={themeDark ? 'main-weather-container-dt' : 'main-light-main-weather-container-lt'}>
       <div className={themeDark ? 'main-weather-form-dt' : 'main-weather-form-lt'}>
-        <ul>
-          {history.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+        <History
+          valueCapture={valueCapture}
+          ref={historyRef}
+        />
 
         <input
           placeholder={languageEng ? 'enter a Location ...' : 'ingrese una ubicación ...'}
@@ -86,6 +65,7 @@ export default function WeatherData() {
 
       {languageEng ? <EnglishCardInfo /> : <SpanishCardInfo />}
 
+      {/* if fetch fn response code status is equal to '404' , redirect to URL path that contains more info about this type error in fetching data */}
       {shouldRedirect ? <Redirect to='/notfound' /> : null}
     </main>
   );
